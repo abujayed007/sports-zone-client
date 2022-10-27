6996af2cf426ea777de584e497528e498cc669ba
abujayed007 <abujayed007@gmail.com>
2022-10-28 03:03:21 +0600

    pdf completed

--- src/Pages/SportsDetails/SportsDetails.js
+++ src/Pages/SportsDetails/SportsDetails.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import { Col, Collapse, Row } from 'react-bootstrap';
-import LeftSide from '../LeftSide/LeftSide';
+import { useReactToPrint } from 'react-to-print'
+
+
 
 
 
 const SportsDetails = () => {
     const sportsDetails = useLoaderData()
 
-    const { name, benifits, title, image } = sportsDetails
+    const { name, benifits, title, image } = sportsDetails;
+
+    const componentRef = useRef()
+
+    const handlePdf = useReactToPrint({
+        content: () => componentRef.current,
+    })
+
 
     return (
-     <Row>
-        
-        <Col className='d-flex justify-content-center'>
-        <Card className="mt-3 w-50">
-                    <Card.Header>{name}</Card.Header>
+        <Row>
+            <Col ref={componentRef} className='d-flex justify-content-center'>
+                <Card className="mt-3 w-50">
+                    <Card.Header>
+                        {name}
+                    </Card.Header>
+                    <button className='bg-info border-none' onClick={handlePdf}>Print Pdf</button>
                     <Card.Img src={image} />
                     <Card.Body>
                         <Card.Title>{title}</Card.Title>
                         <Card.Text>
-                          <strong>Health Benifits:</strong>  {benifits}
+                            <strong>Health Benifits:</strong>  {benifits}
                         </Card.Text>
                     </Card.Body>
                 </Card>
-            
-        </Col>
-     </Row>
+
+            </Col>
+
+        </Row>
+
     );
 };
 
